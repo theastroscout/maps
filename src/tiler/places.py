587201912b530f14e5d99d363754d2137c269fee
Alexander Yermolenko <i@surfy.one>
2024-04-04 14:49:04 +0100

    Sync Places

--- src/tiler/places.py
+++ src/tiler/places.py
@@ -1,9 +1,25 @@
+import pymongo
+
 import osmium
 wkbfab = osmium.geom.WKBFactory()
 from shapely.geometry import Point, Polygon, MultiPolygon, mapping
 import shapely.wkb as wkblib
 
 types = []
+
+db_client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = db_client['Maps']
+places_collection = db['places']
+
+
+def fix_coords(items):
+	result = []
+	for item in items:
+		if isinstance(item, tuple):
+			result.append(fix_coords(item))
+		else:
+			result.append(round(item, 5))
+	return result
 
 class OSM_handler(osmium.SimpleHandler):
 
@@ -12,12 +28,33 @@ class OSM_handler(osmium.SimpleHandler):
 	def node(self, o):
 
 		if 'amenity' in o.tags:
-			print(get_data(o, 'node'))
+			data = get_data(o, 'node') 
+			print(data)
+
+			place = places_collection.find_one({'oid': data['oid']})
+
+			if not place:
+				print('Insert')
+				places_collection.insert_one(data)
+			else:
+				print('Update')
+				# print(place)
+				places_collection.update_one({ '_id': place['_id']}, { '$set': data })
 
 	def area(self, o):
 		
 		if 'amenity' in o.tags:
-			print(get_data(o, 'area'))
+			data = get_data(o, 'area')
+
+			print(data)
+
+			place = places_collection.find_one({'oid': data['oid']})
+
+			if not place:
+				print('Insert')
+				places_collection.insert_one(data)
+			else:
+				print('Update')
 
 def get_data(o, geom_type):
 	amenity = o.tags.get('amenity')
@@ -29,20 +66,53 @@ def get_data(o, geom_type):
 	if not name:
 		name = amenity
 
+	if amenity == 'car_sharing' and 'operator' in o.tags:
+		name = o.tags.get('operator')
+
+	name = parse_name(name)
+
 	location = False
+	geom = False
 	if geom_type == 'node':
 		location = mapping(Point([o.location.lon,o.location.lat]))
 	else:
 		wkbshape = wkbfab.create_multipolygon(o)
-		location = mapping(wkblib.loads(wkbshape, hex=True))
+		geom = wkblib.loads(wkbshape, hex=True)
+		
+		location = mapping(geom.centroid)
 
+		geom = mapping(geom)
+		geom['coordinates'] = fix_coords(geom['coordinates'])
 
-	return {
-		'geom_type': geom_type,
+	location['coordinates'] = fix_coords(location['coordinates'])
+
+	# Search String
+
+	search = [name, parse_name(amenity)]
+
+	'''
+	
+	Create Object
+
+	'''
+
+	obj = {
+		'oid': o.id,
+		# 'geom_type': geom_type,
 		'name': parse_name(name),
 		'type': amenity,
-		'location': location
+		'location': location,
+		'search': ' '.join(search)
 	}
+
+	if geom:
+		if geom['type'] == 'MultiPolygon':
+			if len(geom['coordinates']) == 1 and len(geom['coordinates'][0]) == 1:
+				geom['type'] = 'Polygon'
+				geom['coordinates'] = geom['coordinates'][0][0]
+		obj['geom'] = geom
+
+	return obj
 
 def parse_name(name):
 	name = name.split('_')
